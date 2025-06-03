899b9a83c94cfcf4844d44cc3bbcd69a2468b631
tahiquda <halizameliani@gmail.com>
2025-06-03 23:45:25 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Link ;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\Pegawai2Controller ;
+use App\Http\Controllers\BlogController ;
+// import java.io;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// System.out.print();
+// Disini pakai ::
 
-Route::get('/', function () {
+Route::get('/', function () { // internal controller
     return view('welcome');
 });
+
+Route::get('halo', function () {
+	return "<h2>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h2>";
+});
+
+Route::get('blog', function () {
+	return view('blog'); // tidak ada underline karena belum ada filenya
+});
+
+Route::get('hello', [Link::class, 'helloword']); // menggunakan controller
+
+//ihiihih
+
+
